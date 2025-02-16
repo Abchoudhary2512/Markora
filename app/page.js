be6130b6e8a6224011788f7cef2dfd2a144748a6
ajay2512 <bhakarajay033@gmail.com>
2025-02-16 17:55:31 +0530

    project setup done

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>this is the new projet</h2>
+      <Button>this is the main button</Button>
+    </div>
+  )
+}
